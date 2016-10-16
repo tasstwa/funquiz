b1b910eaddbb8c6a92c46cf12a2cfa1f53dddada
Martin Gadbois <mgadbois@gmail.com>
2016-10-16 09:03:11 -0400

    Wait answer countdown works.

--- funquiz.py
+++ funquiz.py
@@ -7,6 +7,7 @@ import sys
 import json
 import transitions
 import logging
+import time
 
 class Game(transitions.Machine):
     def __init__(self):
@@ -15,11 +16,12 @@ class Game(transitions.Machine):
                    "Test",
                    "AskQuestion",
                    "WaitAnswer",
+                   "Countdown",
                    "WaitJudge",
                    "RightAnswer",
                    "WrongAnswer",
                    "StealAnswer",
-                   "Timeout",
+                   "NoAnswer",
                    "Score"
                  ]
         transitions.Machine.__init__(self,
@@ -35,12 +37,18 @@ class Game(transitions.Machine):
         # Test
         self.add_transition('keypress','Test','AskQuestion',conditions='ready_to_go')
         self.add_transition('hitBuzzer','Test','Test',before=['store_buzzer_status'])
-        self.add_transition('tick','Test','Test',before=['store_buttons'])
+ #       self.add_transition('tick','Test','Test',before=['store_buttons'])
         # AskQuestion
         self.add_transition('keypress','AskQuestion','WaitAnswer')
         # WaitAnswer
         self.add_transition('hitBuzzer','WaitAnswer','WaitJudge')
+        self.add_transition('tick','WaitAnswer','Countdown')
 #        self.add_transition('keypress','WaitAnswer','WaitJudge',conditions='is_buzzer_key')
+        # Countdown
+        self.add_transition('hitBuzzer','Countdown','WaitJudge')
+        self.add_transition('one_second','Countdown','Countdown',before=['display_time_left'])
+        self.add_transition('time_expired',"Countdown","NoAnswer")
+
         self.round = 0
         self.buttons = [ False ] * 8 # Local copy of pressed buttons
         self.screen = Screen()
@@ -70,41 +78,59 @@ class Game(transitions.Machine):
         index = int(event.kwargs['num'])
         self.buzzer_tested[index] = True
         self.buttons = event.kwargs['buttons']
+    def on_enter_AskQuestion(self,event):
+        self.screen.clear()
+        self.screen.addstr(4,3,"Posez la question. Pressez une touche pour attendre la reponse")
+        self.screen.addstr(5,3,"et commencer le compte a rebours.")
     def on_enter_WaitAnswer(self,event):
         self.screen.clear()
-        self.screen.set_title( self.state)
-        self.screen.addstr(4,3,"Posez la question. Pressez une touche pour attendre la reponse.")
-
+        self.screen.addstr(4,3,"On attends la reponse...")
+        self.countdown = 5
+    def display_time_left(self,event):
+        self.countdown -= 1
+        self.screen.addstr(5,6,"%d sec" % self.countdown)
+        if self.countdown <= 0:
+            self.time_expired()
+        
     def on_enter_WaitJudge(self,event):
         player = event.kwargs.get('num',None)
         self.screen.clear()
-        self.screen.set_title( self.state)
-        self.screen.addstr(4,3,"Joueur #%d" % (player,)) 
+        self.screen.addstr(4,3,"Joueur #%d doit a ete le plus vite." % (player,)) 
          
 
 class Screen(object):
     def __init__(self):
-        self.window = curses.initscr()
+        self.stdscr = curses.initscr()
+        self.max_y,self.max_x = self.stdscr.getmaxyx()
         curses.start_color()
         curses.use_default_colors()
         curses.cbreak()
         curses.noecho()
         curses.halfdelay(1)
+        self.stdscr.border()
+        self.stdscr.refresh()
+        self.window = curses.newwin(self.max_y-7,self.max_x-2,6,1)
         self.clear()
-        self.title = ""
+        self.status =  curses.newwin(5,self.max_x-2,1,1)
+        self.status.refresh()
+        self.window.border()
+        self.window.refresh()
     def getch(self):
         return self.window.getch()
     def cleanup(self):
         curses.endwin()
     def set_title(self,title):
         s = '[' + title + ' ' * (32-len(str(title))) + ']'
-        self.addstr(1,3,s)
+        self.status.clear()
+        self.status.addstr(0,3,s)
+        self.status.refresh()
     def addstr(self,*args):
         self.window.addstr(*args)
         self.window.refresh()
     def clear(self):
         self.window.clear()
         self.window.border()
+        self.window.refresh()
 
 
 def feed_events(machine):
@@ -112,8 +138,13 @@ def feed_events(machine):
     fake_button_on = [ 0 ] * 8
     screen = machine.screen
     try:
+        one_second = time.time()
         while True:
             ch = screen.getch()
+            now = time.time()
+            if (now - one_second) > 1.0:
+                machine.one_second(buttons=button_pressed)
+                one_second = now
             if ch == curses.ERR:
                 for i in range(len(fake_button_on)):
                     if fake_button_on[i] == 1:
