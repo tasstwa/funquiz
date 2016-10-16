8c369453e8ae377bec823eb59b69c84acfd63a14
Martin Gadbois <mgadbois@gmail.com>
2016-10-16 15:24:47 -0400

    Complete text-mode funquiz

--- funquiz.py
+++ funquiz.py
@@ -9,6 +9,12 @@ import transitions
 import logging
 import time
 
+default_config = {
+                    "rounds": 5,
+                    "teams": [ "Equipe %d" % (i,) for i in range(1,3) ],
+                    "players": [ (int(round(i/4)),"Joueur #%d" % (i+1,)) for i in range(8)]
+                } 
+
 class Game(transitions.Machine):
     def __init__(self):
         states = [ "Start",
@@ -20,9 +26,8 @@ class Game(transitions.Machine):
                    "WaitJudge",
                    "RightAnswer",
                    "WrongAnswer",
-                   "StealAnswer",
                    "NoAnswer",
-                   "Score"
+                   "Winners"
                  ]
         transitions.Machine.__init__(self,
                 states=states,
@@ -35,34 +40,60 @@ class Game(transitions.Machine):
         # Welcome
         self.add_transition('keypress','Welcome','Test')
         # Test
-        self.add_transition('keypress','Test','AskQuestion',conditions='ready_to_go')
+        self.add_transition('go','Test','AskQuestion')
         self.add_transition('hitBuzzer','Test','Test',before=['store_buzzer_status'])
  #       self.add_transition('tick','Test','Test',before=['store_buttons'])
         # AskQuestion
         self.add_transition('keypress','AskQuestion','WaitAnswer')
+        self.add_transition('tick',"AskQuestion","Winners",conditions="done")
+
         # WaitAnswer
         self.add_transition('hitBuzzer','WaitAnswer','WaitJudge')
-        self.add_transition('tick','WaitAnswer','Countdown')
+        self.add_transition('tick','WaitAnswer','Countdown',after=['display_time_left'])
 #        self.add_transition('keypress','WaitAnswer','WaitJudge',conditions='is_buzzer_key')
         # Countdown
-        self.add_transition('hitBuzzer','Countdown','WaitJudge')
-        self.add_transition('one_second','Countdown','Countdown',before=['display_time_left'])
+        self.add_transition('hitBuzzer','Countdown','WaitJudge',before=['store_who_answered'])
+        self.add_transition('one_second','Countdown','Countdown',before=['dec_timer','display_time_left'])
         self.add_transition('time_expired',"Countdown","NoAnswer")
+        # NoAnswer
+        self.add_transition('keypress',"NoAnswer","AskQuestion")
+       
+        # WaitJudge
+        self.add_transition('yes','WaitJudge','RightAnswer')
+        self.add_transition('no','WaitJudge','WrongAnswer')
 
+        # RightAnswer
+        self.add_transition('keypress',"RightAnswer","AskQuestion")
+
+        # WrongAnswer
+        self.add_transition('keypress',"WrongAnswer","AskQuestion")
+
+        self.read_config()
         self.round = 0
+        self.score = [ 0, 0 ]
         self.buttons = [ False ] * 8 # Local copy of pressed buttons
         self.screen = Screen()
         self.to_Welcome()
 
+    def read_config(self):
+        try:
+            self.config = json.load(file("funquiz.cfg"))
+        except IOError:
+            self.config = default_config
+        self.players = self.config["players"]
+
     def after_state_change(self,event):
+        self.screen.set_status("Ronde %2u/%2u  %s: %2u   %s: %2u" % 
+            (self.round,self.config["rounds"],self.config["teams"][0],self.score[0],self.config["teams"][1],self.score[1]))
         self.screen.set_title( self.state)
         
+    def done(self,event):
+        return self.round > self.config["rounds"]
+ 
     def never(self,event):
         return False
     def store_buttons(self,event):
         self.buttons = event.kwargs['buttons']
-    def ready_to_go(self,event):
-        return event.kwargs.get('key',0) == ord('g')
     def is_buzzer_key(self,event):
         ch = event.kwargs.get('key',0)
         return ch >= ord('1') and ch <= ord('8')
@@ -73,12 +104,14 @@ class Game(transitions.Machine):
         for i in range(len(self.buzzer_tested)):
             s = { True: "OK" , False: "__"}[self.buzzer_tested[i]] 
        #     s = { True: "OK" , False: "  "}[self.buttons[i]] 
-            self.screen.addstr(5 + i,5,"Joueur #%d [%s]" % (i+1,s))
+            self.screen.addstr(5 + i,5,"%s [%s]" % (self.players[i][1],s))
     def store_buzzer_status(self,event):
         index = int(event.kwargs['num'])
         self.buzzer_tested[index] = True
         self.buttons = event.kwargs['buttons']
     def on_enter_AskQuestion(self,event):
+        self.answered_by = None
+        self.round += 1
         self.screen.clear()
         self.screen.addstr(4,3,"Posez la question. Pressez une touche pour attendre la reponse")
         self.screen.addstr(5,3,"et commencer le compte a rebours.")
@@ -86,17 +119,50 @@ class Game(transitions.Machine):
         self.screen.clear()
         self.screen.addstr(4,3,"On attends la reponse...")
         self.countdown = 5
-    def display_time_left(self,event):
+    def dec_timer(self,event):
         self.countdown -= 1
+    def display_time_left(self,event):
         self.screen.addstr(5,6,"%d sec" % self.countdown)
         if self.countdown <= 0:
             self.time_expired()
+    def store_who_answered(self,event):
+        player = event.kwargs.get('num',None)
+        self.answered_by = int(player)
+        
+    def on_enter_NoAnswer(self,event):
+        self.screen.clear()
+        self.screen.addstr(4,3,"Pas de reponse. On continue... pressez une touche.")
         
     def on_enter_WaitJudge(self,event):
         player = event.kwargs.get('num',None)
         self.screen.clear()
-        self.screen.addstr(4,3,"Joueur #%d doit a ete le plus vite." % (player,)) 
+        self.screen.addstr(4,3,"'%s' a ete le plus vite.\n" % (self.players[int(player)][1],)) 
+        self.screen.addstr(5,3,"La reponse est bonne? (O/N)")
          
+    def on_enter_RightAnswer(self,event):
+        self.screen.clear()
+        winning_team = self.players[self.answered_by][0]
+
+        self.screen.addstr(4,3,"Bonne reponse par %s [%s]! On continue... pressez une touche." %
+                 (self.players[self.answered_by][1],self.config["teams"][winning_team]))
+        self.score[winning_team] += 1
+
+    def on_enter_WrongAnswer(self,event):
+        self.screen.clear()
+        losing_team = self.players[self.answered_by][0]
+        self.screen.addstr(4,3,"Mauvaise reponse par %s [%s]! On continue... pressez une touche." %
+                 (self.players[self.answered_by][1],self.config["teams"][losing_team]))
+    def on_enter_Winners(self,event):
+        self.screen.clear()
+        winner = "Aucune!"
+        if self.score[0] > self.score[1]:
+            winner = self.config["teams"][0]
+        elif self.score[1] > self.score[0]:
+            winner = self.config["teams"][1]
+    
+        self.screen.addstr(4,3,"Equipe gagnante: %s... " % winner)
+        self.screen.addstr(5,3,"Taper ESC pour quitter")
+        
 
 class Screen(object):
     def __init__(self):
@@ -119,10 +185,13 @@ class Screen(object):
         return self.window.getch()
     def cleanup(self):
         curses.endwin()
+    def set_status(self,status):
+        self.status_line = status
     def set_title(self,title):
         s = '[' + title + ' ' * (32-len(str(title))) + ']'
         self.status.clear()
         self.status.addstr(0,3,s)
+        self.status.addstr(1,3,self.status_line)
         self.status.refresh()
     def addstr(self,*args):
         self.window.addstr(*args)
@@ -137,6 +206,11 @@ def feed_events(machine):
     button_pressed = [ False ] * 8
     fake_button_on = [ 0 ] * 8
     screen = machine.screen
+    special_keys = {
+            'o': machine.yes,
+            'n': machine.no,
+            'g': machine.go
+            }
     try:
         one_second = time.time()
         while True:
@@ -164,6 +238,9 @@ def feed_events(machine):
                 machine.hitBuzzer(num = index,buttons=button_pressed)
             else:
                 machine.keypress(key=ch,buttons=button_pressed)
+                if chr(ch) in special_keys.keys():
+                    special_keys[chr(ch)](key=ch,buttons=button_pressed)
+
     except:
         raise
     finally:
@@ -179,4 +256,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-    
